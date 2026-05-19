69b9b2f0b0730b67eb62f0b8a80c00e4ae5a3978
CDZk1 <kauacardosogoncalves111@gmail.com>
2026-05-18 21:32:41 -0300

    AULA

---
+++ QUESTÃO116/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+
+    //leia um numero, e divida por 2 (sucessivamente), ate que o resulante seja menor que 1.
+    //mostre a ultima divisão e a quantidade de divisoes feitas.
+
+    float n, resultado;
+    int div_feitas = 0;
+
+    printf("Informe o numero; ");
+    scanf("%f", &n);
+
+    
+
+    return 0;
+}
