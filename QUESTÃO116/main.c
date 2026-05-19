227019b4a27c868a93d304a951ecdb2a6c8c847c
CDZk1 <kauacardosogoncalves111@gmail.com>
2026-05-18 21:44:23 -0300

    AULA

--- QUESTÃO116/main.c
+++ QUESTÃO116/main.c
@@ -5,13 +5,17 @@ int main(){
     //leia um numero, e divida por 2 (sucessivamente), ate que o resulante seja menor que 1.
     //mostre a ultima divisão e a quantidade de divisoes feitas.
 
-    float n, resultado;
+    float n;
     int div_feitas = 0;
 
-    printf("Informe o numero; ");
+    printf("Informe o numero: ");
     scanf("%f", &n);
-
     
+    while (n >= 1){
+        n = n / 2;
+        div_feitas++;
+    }
+    printf("%.2f\n%d\n", n, div_feitas);
 
     return 0;
 }
